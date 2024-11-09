d38b82c3765ce32ec8e7d5a8a854d715860842c5
Serge Bakharev <serge.bakharev@gmail.com>
2024-11-10 09:20:11 +1100

    move debug statements

--- scripts/create-dmg.sh
+++ scripts/create-dmg.sh
@@ -5,17 +5,19 @@ rm -rf ./dist/carveracontroller-community.dmg
 
 # rename .app
 mv "./dist/carveracontroller.app" "./dist/Carvera Controller Community.app"
+ls "./dist/"
 
-echo "Create DMG ..."
+
+echo "Creating DMG ..."
 create-dmg \
     --volname "carvera-controller-community" \
     --background "packaging_assets/dmg_background.jpg" \
     --volicon "packaging_assets/icon-src.icns" \
     --window-pos 200 200 \
     --window-size 460 160 \
-    --icon "carveracontroller.app" 130 130 \
+    --icon "Carvera Controller Community.app" 130 130 \
     --icon-size 64 \
-    --hide-extension "carveracontroller.app" \
+    --hide-extension "Carvera Controller Community.app" \
     --app-drop-link 510 130 \
     --format UDBZ \
     --no-internet-enable \
