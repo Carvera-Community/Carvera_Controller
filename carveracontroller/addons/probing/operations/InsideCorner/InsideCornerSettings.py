0b005d8e100ac2b908f47faef6761c862081f5af
Serge Bakharev <serge.bakharev@gmail.com>
2025-06-18 15:03:58 +1000

    hacky spinner code

--- carveracontroller/addons/probing/operations/InsideCorner/InsideCornerSettings.py
+++ carveracontroller/addons/probing/operations/InsideCorner/InsideCornerSettings.py
@@ -14,6 +14,15 @@ class InsideCornerSettings(BoxLayout):
         super(InsideCornerSettings, self).__init__(**kwargs)
 
     def setting_changed(self, key: str, value: float):
+        # Hacky code to translate between key/value of Kivy Spinner
+        if key == 'ZeroXYPosition':
+            if value == 'Disabled':
+                value = "0"
+            elif value == 'X/Y':
+                value = "1"
+            elif value == 'X/Y/Z':
+                value = "2"
+
         param = getattr(InsideCornerParameterDefinitions, key, None)
         if param is None:
             raise KeyError(f"Invalid key '{key}'")
@@ -23,7 +32,17 @@ class InsideCornerSettings(BoxLayout):
 
     def get_setting(self, key: str) -> str:
         param = getattr(InsideCornerParameterDefinitions, key, None)
+
+        # Hacky code to translate between key/value of Kivy Spinner
+        if key == "ZeroXYPosition" and self.config.get("S"):
+            if self.config["S"] == '0':
+                return 'Disabled'
+            elif self.config["S"] == '1':
+                return 'X/Y'
+            elif self.config["S"] == '2':
+                return 'X/Y/Z'
+        
         return str(self.config[param.code] if param.code in self.config else "")
 
     def get_config(self):
-        return self.config;
+        return self.config
